45ae20b30dd7f8f3e0ed08bc6e5a51e0b5d756d4
ajnirp <prinja@duck.com>
2024-12-22 11:22:08 -0800

    move more stuff into util.py

--- util.py
+++ util.py
@@ -1,2 +1,4 @@
-def IsInBounds(idx, max_idx):
-    return 0 <= idx < max_idx
+# Functions for dealing with 2-D arrays.
+def NumRows(grid): return len(grid)
+def NumCols(grid): return len(grid[0])  # Assumes `grid` is not empty.
+def IsInBounds(idx, max_idx): return 0 <= idx < max_idx
