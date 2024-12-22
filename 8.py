9e8de6cd3df0ba9e5974ef2c77da665efec1a384
ajnirp <prinja@duck.com>
2024-12-22 13:59:09 -0800

    refactor to get 8 ready for part 2

--- 8.py
+++ 8.py
@@ -15,23 +15,29 @@ def FindAntennaLocations(grid):
                 antenna_locations[grid[row][col]].append((row, col))
     return antenna_locations
 
-def GenerateAntinodes(r1, c1, r2, c2, stop_after_first, num_rows, num_cols):
+# For a given pair of antennas, generate all antinodes for them. For part 1,
+# this only generates two antinodes at most. For part 2, this generates as many
+# antinodes in either direction as can fit into the grid.
+def GenerateAntinodes(r1, c1, r2, c2, part_2, num_rows, num_cols):
     if (r1, c1) == (r2, c2):
         raise ValueError("The antennas coincide")
     temp = []
     # Put r1, c1 on top
     if r1 <= r2:
         r1, c1, r2, c2 = r2, c2, r1, c1
+    row_diff = r2 - r1
+    col_diff = c2 - c1
     # r1, c1 => r2, c2 goes down and right
     if c1 < c2:
-        rleft, cleft = r1 - (r2 - r1), c1 - (c2 - c1)
-        rright, cright = r2 + (r2 - r1), c2 + (c2 - c1)
+        rleft, cleft = r1 - row_diff, c1 - col_diff
+        rright, cright = r2 + row_diff, c2 + col_diff
         temp.append((rleft, cleft))
         temp.append((rright, cright))
     # r1, c1 => r2, c2 goes down and left
     else:
-        rleft, cleft = r2 + (r2 - r1), c2 - (c1 - c2)
-        rright, cright = r1 - (r2 - r1), c1 + (c1 - c2)
+        col_diff = -col_diff
+        rleft, cleft = r2 + row_diff, c2 - col_diff
+        rright, cright = r1 - row_diff, c1 + col_diff
         temp.append((rleft, cleft))
         temp.append((rright, cright))
     result = []
@@ -42,7 +48,7 @@ def GenerateAntinodes(r1, c1, r2, c2, stop_after_first, num_rows, num_cols):
 
 
 # Find all antinodes for all antennas.
-def FindAllAntinodes(antenna_locations, stop_after_first, num_rows, num_cols):
+def FindAllAntinodes(antenna_locations, part_2, num_rows, num_cols):
     result = set()
     for antenna, locations in antenna_locations.items():
         if len(locations) < 2:
@@ -51,18 +57,22 @@ def FindAllAntinodes(antenna_locations, stop_after_first, num_rows, num_cols):
             for j in range(i + 1, len(locations)):
                 r1, c1 = locations[i]
                 r2, c2 = locations[j]
-                antinodes = GenerateAntinodes(r1, c1, r2, c2, stop_after_first, num_rows, num_cols)
+                antinodes = GenerateAntinodes(r1, c1, r2, c2, part_2, num_rows, num_cols)
                 for antinode in antinodes:
                     result.add(antinode)
     return result
 
-def Part1And2(grid, stop_after_first, count_antennas):
+def Solve(grid, part_2):
     antenna_locations = FindAntennaLocations(grid)
     kNumRows, kNumCols = NumRows(grid), NumCols(grid)
-    all_antinodes = FindAllAntinodes(antenna_locations, stop_after_first, kNumRows, kNumCols)
-    return len(all_antinodes) + (len(antenna_locations) if count_antennas else 0)
+    all_antinodes = FindAllAntinodes(antenna_locations, part_2, kNumRows, kNumCols)
+    if part_2:
+        for _, location in antenna_locations:
+            all_antinodes.add(location)
+    return len(all_antinodes)
 
 # Part 1
-print(Part1And2(grid, True, False))
+print(Solve(grid, False))
 
 # Part 2
+# print(Solve(grid, True))
