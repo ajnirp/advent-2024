b359f5e9a5874b150c2540ead5f7f54c103df526
ajnirp <prinja@duck.com>
2024-12-22 17:30:39 -0800

    10 both parts

--- 10.py
+++ 10.py
@@ -3,30 +3,32 @@ from util import *
 with open(0) as file:
     grid = [[int(char) for char in line.strip()] for line in file.readlines()]
 
-# Does a breadth-first search from `start` in `grid`. Returns the score for that
-# trailhead.
-def Score(start, grid):
-    start_r, start_c = start
-    if grid[start_r][start_c] != 0:
-        return 0
+# Does a breadth-first search from `start` in `grid`. Returns the score and
+# rating for `start`.
+def Score(start, grid, part_1):
+    if grid[start[0]][start[1]] != 0: return 0, 0
     kNumRows, kNumCols = NumRows(grid), NumCols(grid)
-    queue = [start]
-    peaks = set()
-    result = 0
+    peaks = set()  # Set of all peaks i.e. locations where the value is 9.
+    rating = 0  # Number of paths from `start` to a peak.
+    seen = set()  # Set of all cells seen during the search.
+    queue = [start]  # Breadth-first search queue.
     while queue:
         next_wave = []
         for r, c in queue:
             for nr, nc in Neighbors(r, c):
-                if not (IsInBounds(nr, kNumRows) and IsInBounds(nc, kNumCols)):
-                    continue
+                if not (IsInBounds(nr, kNumRows) and IsInBounds(nc, kNumCols)): continue
                 if not grid[nr][nc] == grid[r][c] + 1: continue
-                if grid[nr][nc] == 9: peaks.add((nr, nc))
+                if part_1 and (nr, nc) in seen: continue
+                if grid[nr][nc] == 9:
+                    peaks.add((nr, nc))
+                    rating += 1
                 else: next_wave.append((nr, nc))
+                if part_1: seen.add((nr, nc))
         queue = next_wave
-    return len(peaks)
+    return len(peaks), rating
 
-def Part1(grid):
+def Solve(grid, part_1):
     kNumRows, kNumCols = NumRows(grid), NumCols(grid)
-    return sum(Score((r, c), grid) for r in range(kNumRows) for c in range(kNumCols))
+    return sum(Score((r, c), grid, part_1)[0 if part_1 else 1] for r in range(kNumRows) for c in range(kNumCols))
 
-print(Part1(grid))
+print(Solve(grid, True), Solve(grid, False))  # 459 1034
