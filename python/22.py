090987449329897dae702828a0078fc1d2d6751b
ajnirp <prinja@duck.com>
2024-12-31 01:08:39 -0800

    22 second part, had to look it up though

--- python/22.py
+++ python/22.py
@@ -1,23 +1,32 @@
+from collections import defaultdict
+
 with open(0) as file:
     numbers = [int(line.strip()) for line in file.readlines()]
 
-def Mix(value, secret):
-    return value ^ secret
+def Step(num):
+    PRUNE_MASK = 16777215
+    num ^= (num << 6) & PRUNE_MASK
+    num ^= (num >> 5) & PRUNE_MASK
+    num ^= (num << 11) & PRUNE_MASK
+    return num
 
-def Prune(secret):
-    return secret % 16777216
+def Solve(numbers):
+    sequences, seen = defaultdict(int), set()
+    part1 = 0
+    for num in numbers:
+        seen = set()
+        window = []
+        for step in range(2000):
+            next_num = Step(num)
+            window.append((next_num % 10) - (num % 10))
+            num = next_num
+            if len(window) == 4:
+                if (sequence := tuple(window)) not in seen:
+                    sequences[sequence] += next_num % 10
+                    seen.add(sequence)
+                window.pop(0)
+        part1 += num
+    part2 = max(sequences.values())
+    return part1, part2
 
-def Step(secret):
-    secret = Prune(Mix(secret * 64, secret))
-    secret = Prune(Mix(secret // 32, secret))
-    return Prune(Mix(secret * 2048, secret))
-
-def Steps(secret, num_steps):
-    for _ in range(num_steps):
-        secret = Step(secret)
-    return secret
-
-def Part1(numbers):
-    return sum(Steps(number, 2000) for number in numbers)
-
-print(Part1(numbers))  # 18525593556
+print(Solve(numbers))  # (18525593556, 2089)
